f42ab3547e68b47d1cb5ff7032eeb7b0a8f011ef
Pasoxn <33795163+Pasoxn@users.noreply.github.com>
2017-12-11 23:16:37 -0500

    1st day
    -made the main circle rotate
    -made the pin able to move and stick onto the main circle and rotate at the same time

---
+++ aa-clone/Assets/Pin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    private bool isPinned = false; //setting up the pin's natural state as not attatched to the big circle 
+
+    public float speed = 20f; //allows the speed to be set, float because movement is involved
+    public Rigidbody2D rb; //making sure the rigidbody is there to interact with 
+
+    void Update()
+    {
+        if(!isPinned) //if it isn't false...(enter thing that happens)
+        rb.MovePosition(rb.position + Vector2.up * speed * Time.deltaTime); //moving the rigidbody, take the coordinates (x,y) and modifying it, coordinates + z speed and just time.deltatime cause speed 
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       if (col.tag == "Rotator") //naming the collider TAG as the rotator 
+        {
+            transform.SetParent(col.transform);
+            isPinned = true; //when it is true, what happens?
+
+        }
+    }
+}
+
+
+
+   
