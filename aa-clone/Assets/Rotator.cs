a95b5458e40fbede149fe6bb1293fef376ee86e2
Pasoxn <33795163+Pasoxn@users.noreply.github.com>
2017-12-15 18:49:17 -0500

    added notes

--- aa-clone/Assets/Rotator.cs
+++ aa-clone/Assets/Rotator.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Rotator : MonoBehaviour
 {
 
-    public float speed = 100f;
+    public float speed = 100f; //setting speed as a variable that has something to do with movement
 
     void Update()
     {
-        transform.Rotate(0f, 0f, speed * Time.deltaTime);
+        transform.Rotate(0f, 0f, speed * Time.deltaTime); //rotates by degrees, 0x, 0y, and z is the speed that can be adjusted cause its a variable
+        //Time.deltaTime makes it so that the game plays the same, no matter if the framerate is low or high
     }
 }
